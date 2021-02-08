f11463718410d9a9e10427533216919775db4fd0
Erik Schake <erik.schake@gmail.com>
2021-02-08 20:23:02 +0200

    Disable pinch2zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.scss';
 import React from 'react';
+import Head from 'next/head';
 
 function OurWorlInNews({ Component, pageProps }) {
 
     const Layout = Component.Layout ? Component.Layout : React.Fragment;
 
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 }
 
