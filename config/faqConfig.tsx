188c4668dca849388286f4661bb5d457e80078d7
Erik Schake <erik.schake@gmail.com>
2021-02-10 08:47:49 +0200

    Line breaks in faq

--- config/faqConfig.tsx
+++ config/faqConfig.tsx
@@ -4,11 +4,11 @@ const { Paragraph } = Typography;
 export default [
     {
         question: "What is this?",
-        answer: <Paragraph>This website tries to visualize what's going on around the world. You can hover or click the countries on the map to see a preview of one top article from or about this country. Then click again to get to see the article full screen.</Paragraph>,
+        answer: <Paragraph>This website tries to visualize what's going on around the world. <br /> You can hover or click the countries on the map to see a preview of one of the country's top articles. Then click again to get to see the article full screen.</Paragraph>,
     },
     {
         question: "Why does it exist?",
-        answer: <Paragraph>We are all living in a bubble. Be it our friends and family circle, twitter or our country's society as a whole. Some time ago I was wondering what other countries in the world are worrying about right now. What is their public debate? Do they have the same problems or totally different ones? From this the idea emerged to fetch and visualize news articles from different countries.</Paragraph>,
+        answer: <Paragraph>We are all living in a bubble. Be it our friends and family circle, twitter or our country's society as a whole.<br /> Some time ago I was wondering what other countries in the world are worrying about right now. What is their public debate? Do they have the same problems or totally different ones?<br /> From this, the idea emerged to fetch and visualize news articles from different countries.</Paragraph>,
     },
     {
         question: "How often are news updated?",
@@ -16,7 +16,7 @@ export default [
     },
     {
         question: "What are the sources?",
-        answer: <Paragraph>The news are fetched once a day by querying the API from <a href="https://thenewsapi.com" target='_blank'>thenewsapi.com</a>. Top stories are selected by them and exposed via the API. They offer differing amounts of news sites where data is fetched from for all 55 countries they offer.</Paragraph>,
+        answer: <Paragraph>The news are fetched once a day by querying the API from <a href="https://thenewsapi.com" target='_blank'>thenewsapi.com</a>.<br /> Top stories are selected by them and exposed via the API. They offer differing amounts of news sites where data is fetched from for all 55 countries they offer.</Paragraph>,
     },
     {
         question: "How are the countries selected that are included?",
@@ -28,7 +28,7 @@ export default [
     },
     {
         question: "Why are some news not directly related to the country they are associated with?",
-        answer: <Paragraph>The selection of the one top story currently solely relies on the top result provided by <a href="https://thenewsapi.com" target='_blank'>thenewsapi.com</a>. This may later be enhanced using semantic interpretation.</Paragraph>,
+        answer: <Paragraph>The selection of the one top story currently solely relies on the top result provided by <a href="https://thenewsapi.com" target='_blank'>thenewsapi.com</a>.<br /> This may later be enhanced using semantic interpretation.</Paragraph>,
     },
     {
         question: "Why is not the entire article text shown and translated?",
